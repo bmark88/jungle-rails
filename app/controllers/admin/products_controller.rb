3cafa1273c2b11c9062fc228bf22c6ff4b782718
bmark88 <bradley.mark@hotmail.com>
2020-04-29 02:59:54 +0000

    Add HTTP basic authentication, credentials located in local .env file

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,7 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
+                               password: ENV["HTTP_BASIC_PASSWORD"],
+                               if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
 
   def index
     @products = Product.order(id: :desc).all
