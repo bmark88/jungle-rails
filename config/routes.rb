f1da5820915134ac2442f75d8c66717e76c2f957
bmark88 <bradley.mark@hotmail.com>
2020-04-29 23:58:45 +0000

    Add routes for session and users authentication to require users to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
   resource :about, only: [:show] do
   end
 
