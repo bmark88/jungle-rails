354fdd36f46b2d39aa34bd744bbb899267a55139
bmark88 <bradley.mark@hotmail.com>
2020-04-30 21:49:40 +0000

    Refactor to have authentication at top level base controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,4 @@
 class Admin::CategoriesController < ApplicationController
-  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
-  password: ENV["HTTP_BASIC_PASSWORD"],
-  if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
 
   def index
     @categories = Category.order(id: :desc).all
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,4 @@
 class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"],
-                               password: ENV["HTTP_BASIC_PASSWORD"],
-                               if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
 
   def index
     @products = Product.order(id: :desc).all
